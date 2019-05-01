7ce3035143da24abde30c3777f3203bfb4ba6642
Вовк Ольга <vovkolga@macbook-vovk.local>
2019-05-01 20:19:47 +0300

    Initial Commit

---
+++ app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Вовк Ольга' on 5/1/19.
+//  Copyright © 2019 Вовк Ольга'. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
