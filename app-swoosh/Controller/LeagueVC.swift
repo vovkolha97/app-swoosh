8ed383c5da25b154914c0ae6ef974ce5a41919bd
Olha Vovk <vovkolha97@gmail.com>
2019-05-19 15:09:13 +0300

    passing data between VC

--- app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/Controller/LeagueVC.swift
@@ -42,5 +42,9 @@ class LeagueVC: UIViewController {
    nextBtn.isEnabled = true
     
     }
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
+    }
 }
